3f6d8cd401a68060f776d777289e5399646c3f13
Ryan Flaherty <rcflaherty@gmail.com>
2015-07-21 11:06:08 -0700

    updated file structure in prep for angular

--- nodetest2/routes/update.js
+++ nodetest2/routes/update.js
@@ -2,11 +2,11 @@
 
 //Edit info per askMPA.com
 exports.updateinfo = function(db) {
-    return function(req, res) {
-        var userToUpdate = req.params.id;
-        var doc = {$set: req.body};
-        db.get('userlist').update({"_id":userToUpdate}, doc, function(err, result) {
-            res.send((result == 1) ? {msg: ''} : {msg: 'Error: ' + err});
-        });
-    }
+  return function(req, res) {
+    var userToUpdate = req.params.id;
+    var doc = {$set: req.body};
+    db.get('userlist').update({"_id":userToUpdate}, doc, function(err, result) {
+        res.send((result == 1) ? {msg: ''} : {msg: 'Error: ' + err});
+    });
+  }
 };
